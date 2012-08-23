a4c0885a6768d0ac2f8448bf582dadeaee740668
Chris Lowder <clowder@gmail.com>
2012-08-23 17:30:07 +0100

    Actually deploying code.

--- lib/capistrano/recipes/deploy/strategy/mason.rb
+++ lib/capistrano/recipes/deploy/strategy/mason.rb
@@ -7,16 +7,44 @@ module Capistrano
   module Deploy
     module Strategy
       class Mason < Copy
-        def build(directory)
+
+        def deploy!
+          copy_cache ? run_copy_cache_strategy : run_copy_strategy
+
+          build!
+          create_revision_file
+          distribute!
+        ensure
+          rollback_changes
+        end
+
+        def build!
           execute "Building with mason" do
-            streaming_system("mason build #{ directory } -t tgz -o #{ File.basename(filename) }")
+            streaming_system("mason build #{ destination } -c ~/.mason/cache/#{configuration[:application]} -t tgz -o #{ filename }")
           end
         end
 
         private
         def streaming_system(cmd)
-          PTY.spawn(cmd) { |stdout, _, _| stdout.each { |line| logger.trace(line) } }
+            PTY.spawn(cmd) { |stdout, _, _|
+              begin
+                stdout.each { |line| logger.trace(line) }
+              rescue Errno::EIO => ex
+                # NOOP: IO stream ended.
+              end
+            }
         end
+
+        def run_copy_cache_strategy
+          copy_repository_to_local_cache
+          copy_cache_to_staging_area
+        end
+
+        def run_copy_strategy
+          copy_repository_to_server
+          remove_excluded_files if copy_exclude.any?
+        end
+
 
         # A struct for representing the specifics of a compression type.
         # Commands are arrays, where the first element is the utility to be
@@ -29,6 +57,10 @@ module Capistrano
           local_tar = configuration[:copy_local_tar] || 'tar'
 
           Compression.new("tar.gz",  [local_tar, 'czf'], [remote_tar, 'xzf'])
+        end
+
+        def decompress_remote_file
+          run "mkdir #{ configuration[:release_path] } && cd #{ configuration[:release_path] } && #{decompress(remote_filename).join(" ")} && rm #{remote_filename}"
         end
       end
     end
